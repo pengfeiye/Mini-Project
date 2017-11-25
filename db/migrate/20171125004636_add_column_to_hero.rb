572342ef596e5b0998b7abb0f99ddc4726ba77b5
pengfei <pengfeiye1994@gmail.com>
2017-11-24 16:50:45 -0800

    added job to hero db

---
+++ db/migrate/20171125004636_add_column_to_hero.rb
@@ -0,0 +1,5 @@
+class AddColumnToHero < ActiveRecord::Migration[5.1]
+  def change
+    add_column :heros, :job, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124235834) do
+ActiveRecord::Schema.define(version: 20171125004636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171124235834) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "job"
     t.index ["user_id"], name: "index_heros_on_user_id"
   end
 

b35835baf4e652d9eea3ff5dadda0f3d3cb45c43
Sherwin32 <sherwin.shann@gmail.com>
2017-11-24 16:52:49 -0800

    basic homepage structure

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+	def index
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Create Success"
+			redirect_to root_path
+		else
+			flash[:error] = "oh no!"
+			redirect_to sign_up_path
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :password)
+	end
+
+end
